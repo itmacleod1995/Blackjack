5d66b92868f0e62d5b043fd1e750f88336c2480e
Ian <imacleod@mail.umw.edu>
2022-07-09 22:34:57 -0400

    Add functionality to where the prompt tells the player if they are under 21, at 21, or over 21

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ function startGame() {
     let card1 = getRandomIntInclusive(2, 11);
     let card2 = getRandomIntInclusive(2, 11);
 
+    if (card1 + card2 < 21) {
+        document.getElementById("result").textContent = "Under 21, draw another card or hold";
+    } else if (card1 + card2 == 21) {
+        document.getElementById("result").textContent = "You got a blackjack!";
+    }
+
     document.getElementById("player-cards").textContent = card1 + ", " + card2;
     currentSum = card1 + card2;
     document.getElementById("sum").textContent = "Sum: " + currentSum;
@@ -21,5 +27,14 @@ function draw() {
     currentCards.textContent = currentCards.textContent + ", " + newCard;
     let sum = document.getElementById("sum");
     currentSum += newCard;
+
+    if (currentSum > 21) {
+        document.getElementById("result").textContent = "Your hand is over 21, you lose :(";
+    } else if (currentSum == 21) {
+        document.getElementById("result").textContent = "You got a blackjack!";
+    } else {
+        document.getElementById("result").textContent = "Under 21, draw another card or hold";
+    }
+
     sum.textContent = "Sum: " + currentSum;
 }
